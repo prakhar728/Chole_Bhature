3dccf4d972c8aaa0407b6b2de5511716fafe0255
Prakhar Ojha <prakhar@getStan.app>
2022-01-09 01:16:17 +0530

    Ui update

--- frontend/src/Wallet/index.js
+++ frontend/src/Wallet/index.js
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers';
 import React, { useState } from 'react';
 import CholeBhature from '../abi/CholeBhature.json'
-
+import './Index.css'
 function Index() {
     const [address, setaddress] = useState('0');
     const [gotAccount, setgotAccount] = useState(false);
@@ -29,59 +29,66 @@ function Index() {
         console.log('Connecting to the wallet');
     }
 
-    const accountChangeHandler =async (newAccount) => {
+    const accountChangeHandler = async (newAccount) => {
         setaddress(newAccount);
         await updateBalance(newAccount);
     }
     const updateBalance = async (newAccount) => {
         // let tempProvider = new ethers.providers.JsonRpcProvider('https://rpc-mumbai.maticvigil.com/v1/8672801189d10b2b2b6d4a3fae5c9e166a94c96f');
-         let tempProvider = new ethers.providers.Web3Provider(window.ethereum);
+        let tempProvider = new ethers.providers.Web3Provider(window.ethereum);
         setprovider(tempProvider);
         // let tempSigner = tempProvider.getSigner('0xc9c59b672d75871adb234eb083b44dba3dd3c19f');
-         let tempSigner = tempProvider.getSigner()
-         await window.ethereum.enable();
+        let tempSigner = tempProvider.getSigner()
+        await window.ethereum.enable();
         setsigner(tempSigner);
-        let tempContract = new ethers.Contract('0x17BDD4610331654e7fC1443BA42d15DD00394C5a',CholeBhature.abi,tempSigner);
-        let writeTempContract = new ethers.Contract('0x17BDD4610331654e7fC1443BA42d15DD00394C5a',CholeBhature.abi,tempProvider)
+        let tempContract = new ethers.Contract('0x17BDD4610331654e7fC1443BA42d15DD00394C5a', CholeBhature.abi, tempSigner);
+        let writeTempContract = new ethers.Contract('0x17BDD4610331654e7fC1443BA42d15DD00394C5a', CholeBhature.abi, tempProvider)
         setwriteContract(writeTempContract);
         setcontract(tempContract);
         let currentBalance = await tempContract.balanceOf(newAccount);
         console.log(currentBalance.toNumber());
         setbalance(currentBalance);
     }
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        contract.transfer(sendAddress,amount)
-        .then(res=>{
-            console.log(res);
-        })
-        .catch(err=>
-            {console.log(err);
+        contract.transfer(sendAddress, amount)
+            .then(res => {
+                console.log(res);
+                settxHash(res)
             })
-        console.log(amount,sendAddress);
+            .catch(err => {
+                console.log(err);
+            })
+        console.log(amount, sendAddress);
+        console.log(provider, signer, settxHash, writeContract);
     }
     return (
-        <div>
-            <div>
-            {gotAccount && `Address is ${address}`}
-            {!gotAccount && <button onClick={connectTotheWallet}>Connect to your Wallet</button>}
-            {error}
+        <div className='WalletWrapper'>
+            <div className='header'>
+                <div className='Heading'>
+                    BHATURE WALLET
+                </div>
+                <div className='addressHolder'>
+                    {gotAccount && `${address}`}
+                    {!gotAccount && <button onClick={connectTotheWallet}>Connect to your Wallet</button>}
+                    {error}
+                </div>
+            </div>
+            <div className='balanceWrapper'>
+                {balance && `${balance} CB`}
             </div>
             <div>
-            {balance && `Balance is ${balance}`}
-            </div>
-            <div>
-                <form>
+                <form className='formWrapper'>
                     <label>Enter the amount</label>
-                    <input value={amount} onChange={(e)=>setamount(e.target.value)}/>
+                    <input value={amount} onChange={(e) => setamount(e.target.value)} />
                     <label>Enter the address</label>
-                    <input value={sendAddress} onChange={(e)=>setsendAddress(e.target.value)}/>
-                    <button type="submit" onClick={handleSubmit}>Submit</button>
+                    <input value={sendAddress} onChange={(e) => setsendAddress(e.target.value)} />
+                    <button type="submit" onClick={handleSubmit}>SEND</button>
                 </form>
             </div>
             <div>
                 {txHash}
-                </div>
+            </div>
         </div>
     )
 }
